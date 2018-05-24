acf797090e2af4b0c81ed7f83cc13d9db1fcab3b
Khoa Nguyen <39357670+AKN0@users.noreply.github.com>
2018-05-24 13:28:14 -0700

    Final Changes
    Only thing not working is Record button,
    to make it touch screen, change it to OnTouch instead of OnAction

--- src/midifckn/SoundModel.java
+++ src/midifckn/SoundModel.java
@@ -2,68 +2,92 @@ package midifckn;
 
 import java.io.File;
 import javafx.scene.media.AudioClip;
-//import javafx.stage.FileChooser;
 
-
+/*
+This class is used to store, call, and write AudioFile for the program
+*/
 public class SoundModel {
     
+    /////////////////////////////////////FIELD/////////////////////////////////////
     //1st Row Sound
-    private AudioClip s0 = new AudioClip(new File("src\\midifckn\\soundFiles\\808Blaster.wav").toURI().toString());
-    private AudioClip s1 = new AudioClip(new File("src\\midifckn\\soundFiles\\808Kick2.wav").toURI().toString());
-    private AudioClip s2 = new AudioClip(new File("src\\midifckn\\soundFiles\\snareDry.wav").toURI().toString());
-    private AudioClip s3 = new AudioClip(new File("src\\midifckn\\soundFiles\\snareCrusher.wav").toURI().toString());
-    private AudioClip s4 = new AudioClip(new File("src\\midifckn\\soundFiles\\snareBamboo.wav").toURI().toString());
+    private AudioClip s0 = new AudioClip(new File("src/midifckn/soundFiles/808Blaster.wav").toURI().toString());
+    private AudioClip s1 = new AudioClip(new File("src/midifckn/soundFiles/bongo.wav").toURI().toString());
+    private AudioClip s2 = new AudioClip(new File("src/midifckn/soundFiles/snareDry.wav").toURI().toString());
+    private AudioClip s3 = new AudioClip(new File("src/midifckn/soundFiles/snareCrusher.wav").toURI().toString());
+    private AudioClip s4 = new AudioClip(new File("src/midifckn/soundFiles/snareBamboo.wav").toURI().toString());
     //2nd Row Sound
-    private AudioClip s5 = new AudioClip(new File("src\\midifckn\\soundFiles\\808Kick.wav").toURI().toString());
-    private AudioClip s6 = new AudioClip(new File("src\\midifckn\\soundFiles\\808Kick4D#.wav").toURI().toString());
-    private AudioClip s7 = new AudioClip(new File("src\\midifckn\\soundFiles\\808KickE.wav").toURI().toString());
-    private AudioClip s8 = new AudioClip(new File("src\\midifckn\\soundFiles\\snareSoft.wav").toURI().toString());
-    private AudioClip s9 = new AudioClip(new File("src\\midifckn\\soundFiles\\snareLoud.wav").toURI().toString());
+    private AudioClip s5 = new AudioClip(new File("src/midifckn/soundFiles/808Kick.wav").toURI().toString());
+    private AudioClip s6 = new AudioClip(new File("src/midifckn/soundFiles/808Kick4D#.wav").toURI().toString());
+    private AudioClip s7 = new AudioClip(new File("src/midifckn/soundFiles/808KickE.wav").toURI().toString());
+    private AudioClip s8 = new AudioClip(new File("src/midifckn/soundFiles/snareSoft.wav").toURI().toString());
+    private AudioClip s9 = new AudioClip(new File("src/midifckn/soundFiles/snareLoud.wav").toURI().toString());
    //3rd Row Sound
-    private AudioClip s10 = new AudioClip(new File("src\\midifckn\\soundFiles\\closedHiHat.wav").toURI().toString());
-    private AudioClip s11 = new AudioClip(new File("src\\midifckn\\soundFiles\\cymbalRide1.wav").toURI().toString());
-    private AudioClip s12 = new AudioClip(new File("src\\midifckn\\soundFiles\\cymbalRide2.wav").toURI().toString());
-    private AudioClip s13 = new AudioClip(new File("src\\midifckn\\soundFiles\\crash.wav").toURI().toString());
-    private AudioClip s14 = new AudioClip(new File("src\\midifckn\\soundFiles\\crashDistorted.wav").toURI().toString());
+    private AudioClip s10 = new AudioClip(new File("src/midifckn/soundFiles/closedHiHat.wav").toURI().toString());
+    private AudioClip s11 = new AudioClip(new File("src/midifckn/soundFiles/cymbalRide1.wav").toURI().toString());
+    private AudioClip s12 = new AudioClip(new File("src/midifckn/soundFiles/cymbalRide2.wav").toURI().toString());
+    private AudioClip s13 = new AudioClip(new File("src/midifckn/soundFiles/crash.wav").toURI().toString());
+    private AudioClip s14 = new AudioClip(new File("src/midifckn/soundFiles/crashDistorted.wav").toURI().toString());
     //4th Row Sound
-    private AudioClip s15 = new AudioClip(new File("src\\midifckn\\soundFiles\\chordAM.wav").toURI().toString());
-    private AudioClip s16 = new AudioClip(new File("src\\midifckn\\soundFiles\\chordF#M.wav").toURI().toString());
-    private AudioClip s17 = new AudioClip(new File("src\\midifckn\\soundFiles\\guitarWowChord.wav").toURI().toString());
-    private AudioClip s18 = new AudioClip(new File("src\\midifckn\\soundFiles\\cowbell.wav").toURI().toString());     
-    private AudioClip s19 = new AudioClip(new File("src\\midifckn\\soundFiles\\clap2.wav").toURI().toString());
+    private AudioClip s15 = new AudioClip(new File("src/midifckn/soundFiles/chordAM.wav").toURI().toString());
+    private AudioClip s16 = new AudioClip(new File("src/midifckn/soundFiles/chordF#M.wav").toURI().toString());
+    private AudioClip s17 = new AudioClip(new File("src/midifckn/soundFiles/guitarWowChord.wav").toURI().toString());
+    private AudioClip s18 = new AudioClip(new File("src/midifckn/soundFiles/cowbell.wav").toURI().toString());     
+    private AudioClip s19 = new AudioClip(new File("src/midifckn/soundFiles/clap2.wav").toURI().toString());
    //5th Row Sound
-    private AudioClip s20 = new AudioClip(new File("src\\midifckn\\soundFiles\\808Blaster.wav").toURI().toString());
-    private AudioClip s21 = new AudioClip(new File("src\\midifckn\\soundFiles\\808Blaster.wav").toURI().toString());     
-    private AudioClip s22 = new AudioClip(new File("src\\midifckn\\soundFiles\\808Blaster.wav").toURI().toString());
-    private AudioClip s23 = new AudioClip(new File("src\\midifckn\\soundFiles\\808Blaster.wav").toURI().toString());
-    private AudioClip s24 = new AudioClip(new File("src\\midifckn\\soundFiles\\808Blaster.wav").toURI().toString());  
+    private AudioClip s20 = new AudioClip(new File("src/midifckn/soundFiles/TrumpetHarmonies.wav").toURI().toString());
+    private AudioClip s21 = new AudioClip(new File("src/midifckn/soundFiles/sax.wav").toURI().toString());     
+    private AudioClip s22 = new AudioClip(new File("src/midifckn/soundFiles/shaker.wav").toURI().toString());
+    private AudioClip s23 = new AudioClip(new File("src/midifckn/soundFiles/classicImpact.wav").toURI().toString());
+    private AudioClip s24 = new AudioClip(new File("src/midifckn/soundFiles/KSHMRVocal.wav").toURI().toString());  
     //Tracks
-    private AudioClip t0 = new AudioClip(new File("src\\midifckn\\soundFiles\\leadPopsLoops.wav").toURI().toString());
-    private AudioClip t1 = new AudioClip(new File("src\\midifckn\\soundFiles\\majorPluckCmLoop.wav").toURI().toString());     
-    private AudioClip t2 = new AudioClip(new File("src\\midifckn\\soundFiles\\trapLoop4.wav").toURI().toString());
-    private AudioClip t3 = new AudioClip(new File("src\\midifckn\\soundFiles\\organLead.wav").toURI().toString());
-    private AudioClip t4 = new AudioClip(new File("src\\midifckn\\soundFiles\\drop.wav").toURI().toString()); 
+    private AudioClip t0 = new AudioClip(new File("src/midifckn/soundFiles/leadPopsLoops.wav").toURI().toString());
+    private AudioClip t1 = new AudioClip(new File("src/midifckn/soundFiles/shakerLoop.wav").toURI().toString());     
+    private AudioClip t2 = new AudioClip(new File("src/midifckn/soundFiles/trapLoop4.wav").toURI().toString());
+    private AudioClip t3 = new AudioClip(new File("src/midifckn/soundFiles/organLead.wav").toURI().toString());
+    private AudioClip t4 = new AudioClip(new File("src/midifckn/soundFiles/drop.wav").toURI().toString());
     
     
-    private AudioClip[] sampleGrid = {
+    private final AudioClip[] sampleGrid = {
         s0, s1, s2, s3, s4,
         s5, s6, s7, s8, s9,
         s10, s11, s12, s13, s14,
         s15, s16, s17, s18, s19,
         s20, s21, s22, s23, s24
     };
-    private AudioClip[] trackGrid = {t0, t1, t2, t3, t4};
+    private final AudioClip[] trackGrid = {t0, t1, t2, t3, t4};
     
     private double savedVolume = 0;
     
-    public AudioClip getButtonSound(int choice){
+    
+    
+    ////////////////////////////////////METHODS//////////////////////////////////////////
+    public AudioClip getButtonSound(int choice){ // This is our getter
         return sampleGrid[choice];
     }
     
-    public AudioClip getTrackSound(int choice) {
+    public AudioClip getTrackSound(int choice) {// This is the track getter
         return trackGrid[choice];
     }
-    
+    //This is to the recordx50 button as a gag for clicking too many times
+    public double changeVolume() {
+        for (int i = 0; i < 25; i++) {
+            sampleGrid[i].setVolume(-1000);
+        }
+        for (int i = 0; i < 5; i++) {
+                trackGrid[i].setVolume(-1000);
+        }
+        return sampleGrid[0].getVolume();
+    }
+    /**
+     * This method is called on in the action events for the controller and overloads the other method
+     * UP = 0, DOWN = 1, Mute = 2, Unmute = 3
+     * When 0 is called, it loops through both sample and track array, and increase all volume
+     * When 1 is called, it decreases all volume of all AudioClips
+     * When 2 is called, it sets all AudioClip volume to zero, and stores the previous volume in a variable
+     * When 3 is called, it sets all AudioClip back to the previous volume stored in variable
+     * @param choice this will let the program know if volume decreases, increases, mute, or unmute
+     * @return the current volume of all the tracks
+     */
     public double changeVolume(int choice) {
         double h = sampleGrid[0].getVolume();
         if (choice == 1 && h < 1) {
@@ -99,21 +123,57 @@ public class SoundModel {
                 trackGrid[i].setVolume(savedVolume);
             }
         }
-        else if (choice == 4) {
-            for (int i = 0; i < 25; i++) {
-                sampleGrid[i].setVolume(-1000);
-            }
-            for (int i = 0; i < 5; i++) {
-                trackGrid[i].setVolume(-1000);
-            }
-        }
         return sampleGrid[0].getVolume();
     }
     
-    /*public String loadSound(int choice) {
-        FileChooser file1 = new FileChooser();
-        AudioClip s26 = new AudioClip(file1);
-        sampleGrid[choice] = s26;
-        return "New sound added to button";
-    }*/
+    
+    
+    
+    /**
+     * This method is used for replacing the sound within one of the sound buttons, or a setter
+     * It uses a try catch for exception handling. All the strings from the sample sounds should be able to convert to integers
+     * If it cannot be converted to an integer, it will have a number format exception, and it will be a Track button.
+     * If the user loads in another file thats not an audio file, the exception handling catch will notify the user in the result label
+     * If no buttons were selected, it will also notify the user
+     * @param choice converted to integer to determine which button is used, if NumberFormatException, itll determine the track button
+     * @param pathChoice This is the string for the chosen file path
+     * @return returns a string to display on the result panel
+     */
+    public String loadSound(String choice, String pathChoice) {
+        int a;
+        try { //If the combobox choice was 0-24
+            a = Integer.parseInt(choice);
+            AudioClip ac = new AudioClip(new File(pathChoice).toURI().toString());
+            sampleGrid[a] = ac;
+            return "New sound added to button" + a;
+        }
+        catch (NumberFormatException nfe) { //If the string cant be converted to integer, choice was T1-T5
+            switch (choice) {
+                case "T1":
+                    a = 0;
+                    break;
+                case "T2":
+                    a = 1;
+                    break;
+                case "T3":
+                    a = 2;
+                    break;
+                case "T4":
+                    a = 3;
+                    break;
+                case "T5":
+                    a = 4;
+                    break;
+                default:
+                    //If no combobox selection was made
+                    return "Please select a button to replace";
+            }
+            AudioClip ac = new AudioClip(new File(pathChoice).toURI().toString());
+            trackGrid[a] = ac;
+            return "New sound added to track" + (a+1);
+        }
+        catch (Exception e) { //If the file extension wanted audio file
+            return "Please select a proper Audio file";
+        }
+    }
 }
